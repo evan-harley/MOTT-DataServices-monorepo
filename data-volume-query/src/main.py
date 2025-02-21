97ca9baa14984f0afd546bfc6eb779d0db35ebb1
Evan <evan.harley@gov.bc.ca>
2025-02-21 09:23:44 -0800

    Used the getpass function to get the password.

--- data-volume-query/src/main.py
+++ data-volume-query/src/main.py
@@ -1,9 +1,15 @@
+import getpass
 import pandas as pd
 import cx_Oracle
 from tqdm.auto import tqdm
 
 
 def get_oracle_summary_data(conn: cx_Oracle.Connection, schema_name: str = "APP_CPS"):
+    """
+    :param conn: cx_Oracle.Connection -> An open connection to an oracle database as created by cx_Oracle.
+    :param schema_name: str -> The name of the schema which needs the Data Volume Summary to be generated.
+    :return: None -> This function does not return anything as it creates an output file.
+    """
     data = []
     with conn.cursor() as cursor:
         for row in cursor.execute(f'''
@@ -47,16 +53,20 @@ def get_oracle_summary_data(conn: cx_Oracle.Connection, schema_name: str = "APP_
     pass
 
 if __name__ == '__main__':
-
+    # This block gets input from the user. Probably refactoring this to leverage a .env file would be
+    # a better way to go about it. But, I didn't want to add too many layers of complexity.
     user_name = input("Please enter your username: ")
-    password = input("Please enter your password: " )
+    password = getpass.getpass(prompt="Please enter your password: " )
     host = input("Please enter the host uri for the database: ")
     port = input("Please enter the port number the server is listening on: ")
     service = input("Please enter the name of the service (eg. PRD11): ")
     schema = input("Please enter the schema name: ")
 
+    # Creates a formatted string which cx_Oracle connect will recognise as a connection string.
     dsn = cx_Oracle.makedsn(host, port, service)
+    # Creates a connection object to the database using the connection string, username and password provided by the user
     with cx_Oracle.connect(user_name, password, dsn) as connection:
+        # Calls the oracle related summary function.
         get_oracle_summary_data(connection, schema)
 
 
